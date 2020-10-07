747d7e75b3806862e1ca32b8fbcb74a4d1572ab1
msmith2919 <59847525+msmith2919@users.noreply.github.com>
2020-10-07 17:18:28 -0400

    css changes

--- src/components/Box.js
+++ src/components/Box.js
@@ -9,11 +9,13 @@ function Box(props){
     const [img]=React.useState(props.img);
 
     return(
-        <div class={"box"}>
+        <div class={"container"}>
+            <div class={"item"}>
             <h1>Name: {name}</h1>
             <h2>Category: {category}</h2>
             <h3>Special Moves: {value}</h3>
-            <img src={img} alt="image"/>
+            <img src={img} alt="Funko"/>
+            </div>
         </div>
     )
 }
